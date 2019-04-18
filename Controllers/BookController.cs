fa5c62d96ba7a929efd9d57ba99b8ffa8bd88542
SiyandaDlamin <47412827+SiyandaDlamin@users.noreply.github.com>
2019-04-18 13:28:24 +0200

    Add files via upload

---
+++ Controllers/BookController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppAPI_Book.Models;
+
+namespace WebAppAPI_Book.Controllers
+{
+    public class BookController : ApiController
+    {
+        Book[] books = new Book[]
+        {
+
+            new Book(){Id=1,Author="Billy Goat", Price=100,Title="Spider without Duty"},
+            new Book(){Id=2,Author="Siyanda Dlamini", Price=200,Title="The Climb"},
+            new Book(){Id=3,Author="Bafo Khanyeza", Price=100,Title="Accounting"}
+        };
+        // GET: api/Book
+        public IHttpActionResult Get()
+        {
+            return Ok(books);
+        }
+
+        // GET: api/Book/5
+        public IHttpActionResult Get(int id)
+        {
+            Book book = books.FirstOrDefault<Book>(B => B.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        // POST: api/Book
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Book/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Book/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
